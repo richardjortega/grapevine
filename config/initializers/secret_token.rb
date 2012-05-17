d2a26de142fe0cbbc04c348d2c320ffad87dd4aa
Richard Ortega <richardjortega@gmail.com>
2012-05-17 02:55:53 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Grapevine::Application.config.secret_token = '607a4491b6fad839c9cefe495106c5c7070f5208bfa245e586e42ba40bd00872684de6b5e1ff0011d9182883aa50ef414dd72e136c9f29ab707bcd50efb5a5fc'
