108b70a71e4b12a54839bc4b58a45becd35a9363
Richard Ortega <richardjortega@gmail.com>
2012-07-25 21:28:09 -0500

    refreshed migrations

--- db/migrate/20120724005912_removecolumns.rb
+++
@@ -1,8 +0,0 @@
-class Removecolumns < ActiveRecord::Migration
-  def up
-  	remove_column :users, :phone_number
-  end
-
-  def down
-  end
-end
--- db/migrate/20120724224215_add_columns.rb
+++
@@ -1,8 +0,0 @@
-class AddColumns < ActiveRecord::Migration
-  def up
-  	add_column :users, :phone_number, :string
-  end
-
-  def down
-  end
-end
