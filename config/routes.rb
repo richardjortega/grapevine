ae7e761f04948024b87598f8cdcd6494b2405a60
Richard Ortega <richardjortega@gmail.com>
2012-10-17 15:27:15 -0500

    add registrations controller to override Devise Controller on update action to allow user edit profile without password

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Grapevine::Application.routes.draw do
   
   mount StripeEvent::Engine => "/stripe_event"
 
-  devise_for  :users
+  devise_for  :users, :controllers => {:registrations => "registrations"}
   resources   :subscriptions
   resources   :wantmore,    only: [:show], :controller => 'blasts'
   #for testing multiple variations of wantmore page
@@ -37,8 +37,22 @@ Grapevine::Application.routes.draw do
     post 'sign_in' => 'devise/sessions#create', :as => :user_session
     match 'sign_out' => 'devise/sessions#destroy', :as => :destroy_user_session,
       :via => Devise.mappings[:user].sign_out_via
-    get 'edit_profile' => 'devise/registrations#edit', :as => :edit_user_registration
+    get 'edit_profile' => 'registrations#edit', :as => :edit_user_registration
   end
 
+  # default rake routes for devise User
+  #         new_user_session GET    /users/sign_in(.:format)       devise/sessions#new
+  #             user_session POST   /users/sign_in(.:format)       devise/sessions#create
+  #     destroy_user_session DELETE /users/sign_out(.:format)      devise/sessions#destroy
+  #            user_password POST   /users/password(.:format)      devise/passwords#create
+  #        new_user_password GET    /users/password/new(.:format)  devise/passwords#new
+  #       edit_user_password GET    /users/password/edit(.:format) devise/passwords#edit
+  #                          PUT    /users/password(.:format)      devise/passwords#update
+  # cancel_user_registration GET    /users/cancel(.:format)        devise/registrations#cancel
+  #        user_registration POST   /users(.:format)               devise/registrations#create
+  #    new_user_registration GET    /users/sign_up(.:format)       devise/registrations#new
+  #   edit_user_registration GET    /users/edit(.:format)          devise/registrations#edit
+  #                          PUT    /users(.:format)               devise/registrations#update
+  #                          DELETE /users(.:format)               devise/registrations#destroy
   
 end
