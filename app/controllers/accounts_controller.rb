39444bc93429b033fb05a31fd985b98da05efe07
Richard Ortega <richardjortega@gmail.com>
2012-10-21 05:17:23 -0500

    payment form now dependent on users status info, end trial is only submitted for trialing users who want to upgrade

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -4,6 +4,8 @@ class AccountsController < ApplicationController
       @plan = current_user.subscription.plan
       @location = current_user.locations[0]
       @subscription = current_user.subscription
+      @status = current_user.subscription.status_info
+
       
 	end
 
