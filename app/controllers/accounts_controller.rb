beb8634fa7e7755438f2789adb66cfd08bfe2daf
Richard Ortega <richardjortega@gmail.com>
2012-11-02 17:36:13 -0500

    humanize start date in account admin

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -5,6 +5,7 @@ class AccountsController < ApplicationController
       @location = current_user.locations[0]
       @subscription = current_user.subscription
       @status = current_user.subscription.status_info
+      @start_date = Time.at(@subscription.start_date).to_date.strftime("%A, %B %d, %Y")
 	end
 
 end
