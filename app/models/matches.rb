91e0a4f5be43f349feb714711b534bdba53d04e8
Richard Ortega <richardjortega@gmail.com>
2012-12-06 16:26:52 -0600

    add engine routing for devise, remove matches model

--- app/models/matches.rb
+++
@@ -1,8 +0,0 @@
-class Matches < ActiveRecord::Base
-  attr_accessible :overall_rating, 
-  				  :review_id, 
-  				  :source_id, 
-  				  :source_refer_id
-  belongs_to :location
-  belongs_to :source
-end
