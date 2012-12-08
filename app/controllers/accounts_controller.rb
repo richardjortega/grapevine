29626cca83a6298964d9d947592320e55202ab3c
Josh Seltzer <joshseltzer@gmail.com>
2012-12-07 18:25:49 -0600

    added error handling in case for some reason user subscription and plan doesnt have full info

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -4,8 +4,19 @@ class AccountsController < ApplicationController
       @plan = current_user.subscription.plan
       @location = current_user.locations[0]
       @subscription = current_user.subscription
-      @status = current_user.subscription.status_info
-      @start_date = Time.at(@subscription.start_date).to_date.strftime("%A, %B %d, %Y")
+
+      if current_user.subscription.status_info.nil?
+      	@status = "No status info available, please contact Grapevine"
+      else
+      	@status = current_user.subscription.status_info.capitalize
+      end
+
+      if @subscription.start_date.nil?
+      	@start_date = "No start date found, please contact Grapevine"
+      else
+      	@start_date = Time.at(@subscription.start_date).to_date.strftime("%A, %B %d, %Y")
+      end
+      
 	end
 
 end
