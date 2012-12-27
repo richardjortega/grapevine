04c8d252dea575faa700304ddd4ce98d44d1d05c
Richard Ortega <richardjortega@gmail.com>
2012-12-27 01:49:24 -0600

    add plans and users resources, plan is uneditable, users needs work

--- app/admin/plans.rb
+++ app/admin/plans.rb
@@ -1,3 +1,20 @@
 ActiveAdmin.register Plan do
-  
+  index do
+  	h2 :style => "line-height:26px; width:65%;" do 
+  		'This page is not directly editable, plans must be created and updated at Stripe. Changes will then be reflected in our system (soon!, needs to be)'
+  	end
+  	column :name
+  	column :identifier
+  	column :amount do |plan|
+		# Converts cents to dollars
+		sprintf('$%0.2f', plan.amount.to_f / 100.0).gsub(/(\d)(?=(\d\d\d)+(?!\d))/, "\\1,")	
+  	end
+  	column :currency
+  	column :interval
+  	column :trial_period_days
+  	column :location_limit
+  	column :review_limit
+  	column :created_at
+  	column :updated_at
+  end
 end
--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,3 +1,10 @@
 ActiveAdmin.register User do
-  
+  index do
+  	selectable_column
+  	column :first_name
+  	column :last_name
+  	column :email
+  	column :phone_number
+  	default_actions
+  end
 end
