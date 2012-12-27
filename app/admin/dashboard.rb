952fe27e9c263c08b4269aa42dc4419e8a875223
Richard Ortega <richardjortega@gmail.com>
2012-12-27 01:24:46 -0600

    modified the dashboard to show simple counting of last recent users and rest

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -2,32 +2,57 @@ ActiveAdmin.register_page "Dashboard" do
 
   menu :priority => 1, :label => proc{ I18n.t("active_admin.dashboard") }
 
-  content :title => proc{ I18n.t("active_admin.dashboard") } do
+  content :title => 'Grapevine Dashboard' do
     div :class => "blank_slate_container", :id => "dashboard_default_message" do
       span :class => "blank_slate" do
-        span I18n.t("active_admin.dashboard_welcome.welcome")
-        small I18n.t("active_admin.dashboard_welcome.call_to_action")
+        span "This our dashboard... it's literally awsum"
       end
     end
 
-    # Here is an example of a simple dashboard with columns and panels.
-    #
-    # columns do
-    #   column do
-    #     panel "Recent Posts" do
-    #       ul do
-    #         Post.recent(5).map do |post|
-    #           li link_to(post.title, admin_post_path(post))
-    #         end
-    #       end
-    #     end
-    #   end
+    br
+    
+    # Sample data points we may want to collect
+    columns do
+        column do
+            panel 'Total Users' do
+                "#{User.all.count}"
+            end
+            panel 'Recent Users' do
+                ul do
+                    User.last(5).map do |user|
+                        li link_to(user.email, admin_user_path(user))
+                    end
+                end
+            end
+        end
 
-    #   column do
-    #     panel "Info" do
-    #       para "Welcome to ActiveAdmin."
-    #     end
-    #   end
-    # end
+        column do
+            panel 'Total Locations' do
+                "#{Location.all.count}"
+            end
+             panel 'Recent Locations' do
+                ul do
+                    Location.last(5).map do |location|
+                        li link_to(location.name, admin_location_path(location))
+                    end
+                end
+            end
+        end
+
+        column do
+            panel 'Total Reviews' do
+                "#{Location.all.count}"
+            end
+             panel 'Recent Reviews' do
+                ul do
+                    Review.last(5).map do |review|
+                        comment = truncate("#{review.comment}", :length => 50)
+                        li link_to(comment, admin_review_path(review))
+                    end
+                end
+            end
+        end
+    end
+    
   end # content
 end
