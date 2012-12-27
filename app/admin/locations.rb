2946b0edf8f3edd46581b305b9944b363dc63666
Richard Ortega <richardjortega@gmail.com>
2012-12-27 01:53:37 -0600

    fix routing paths for location and vine view paths

--- app/admin/locations.rb
+++ app/admin/locations.rb
@@ -2,7 +2,7 @@ ActiveAdmin.register Location do
 	index do
 		selectable_column
 		column :name do |location|
-			link_to "#{location.name}", "/admin/locations/#{location.id}"
+			link_to "#{location.name}", admin_location_path(location)
 		end
 		column :street_address
 		column :address_line_2
--- app/admin/vines.rb
+++ app/admin/vines.rb
@@ -2,10 +2,10 @@ ActiveAdmin.register Vine do
   index do
   	selectable_column
   	column :source_id do |vine|
-  		link_to "#{vine.source.name}", "/admin/sources/#{vine.source_id}"
+  		link_to "#{vine.source.name}", admin_source_path(vine.source)
   	end
   	column :location_id do |vine|
-  		link_to "#{vine.location.name}", "/admin/locations/#{vine.location_id}"
+  		link_to "#{vine.location.name}", admin_location_path(vine.location)
   	end
   	column :source_location_uri
   	default_actions
