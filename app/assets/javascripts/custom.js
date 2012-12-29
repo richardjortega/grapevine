d9a0030edcb37d36bfe3942436bbfa41522786f1
Josh Seltzer <joshseltzer@gmail.com>
2012-12-28 23:45:48 -0600

    footer overhaul - moved twitter to hero break, layout now 2 cols, testimonial with photo added

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -11,4 +11,9 @@ $(document).ready(function() {
       $(".alert").fadeOut("slow"); 
     }, 1500 ); 
 
+  // twitter call
+  $.getJSON("https://api.twitter.com/1/statuses/user_timeline/pickgrapevine.json?count=1&include_rts=1&callback=?", function(data) {
+     $("#twitter").html(data[0].text);
+});
+
 });
