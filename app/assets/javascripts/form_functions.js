d66f465556908e7aaa637d65f9b2f2917b237b5b
Josh Seltzer <joshseltzer@gmail.com>
2012-12-31 14:37:50 -0600

    replace state fields with select box, do some shit on the dash with upgrade form, added state helper, added zip mask on signup

--- app/assets/javascripts/form_functions.js
+++ app/assets/javascripts/form_functions.js
@@ -1,29 +1,30 @@
 /* Custom JS */
 
 $(document).ready(function() {
-	/* Google Maps Address Picker */
-	var addresspickerMap = $( "#addresspicker_map" ).addresspicker({
-			regionBias: "us",
-			mapOptions: {
-				zoom: 15,
-				center: new google.maps.LatLng(29.4286101, -98.49193479999997)
+	// /* Google Maps Address Picker */
+	// var addresspickerMap = $( "#addresspicker_map" ).addresspicker({
+	// 		regionBias: "us",
+	// 		mapOptions: {
+	// 			zoom: 15,
+	// 			center: new google.maps.LatLng(29.4286101, -98.49193479999997)
 				
-			},
-			  elements: {
-			    map:      "#map",
-			    lat:      "#lat",
-			    lng:      "#lng",
-			    locality: '#locality',
-			    country:  '#country',
-	        type:    '#type' 
-			  },
-			  draggableMarker: false
-		});
-		var gmarker = addresspickerMap.addresspicker( "marker");
-		gmarker.setVisible(true);
-		addresspickerMap.addresspicker( "updatePosition");
+	// 		},
+	// 		  elements: {
+	// 		    map:      "#map",
+	// 		    lat:      "#lat",
+	// 		    lng:      "#lng",
+	// 		    locality: '#locality',
+	// 		    country:  '#country',
+	//         type:    '#type' 
+	// 		  },
+	// 		  draggableMarker: false
+	// 	});
+	// 	var gmarker = addresspickerMap.addresspicker( "marker");
+	// 	gmarker.setVisible(true);
+	// 	addresspickerMap.addresspicker( "updatePosition");
 
-	/* Jquery masked plugin for user's phone number */
+	/* Jquery masked plugin */
 	$("#user_phone_number").mask("(999) 999-9999");
+	$(".zip").mask("99999");
 
 });
