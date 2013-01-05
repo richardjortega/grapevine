3351b6dc2f234f80258d7b3c54374b76d0ca7348
Josh Seltzer <joshseltzer@gmail.com>
2013-01-04 18:57:15 -0600

    contact form client side validation

--- app/assets/javascripts/form_functions.js
+++ app/assets/javascripts/form_functions.js
@@ -5,4 +5,7 @@ $(document).ready(function() {
 	$("#user_phone_number").mask("(999) 999-9999");
 	$("#user_locations_attributes_0_zip").mask("99999");
 
+	// jquery validate contact form
+	$(".contact-form").validate();
+
 });
