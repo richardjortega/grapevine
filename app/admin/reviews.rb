39ae611c08cf736893dcc8def917de1ca05a252a
Richard Ortega <richardjortega@gmail.com>
2013-01-10 03:23:11 -0600

    location name added to reviews page

--- app/admin/reviews.rb
+++ app/admin/reviews.rb
@@ -4,6 +4,9 @@ ActiveAdmin.register Review do
 		h2 :style => "line-height:26px; width:65%;" do 
 			'This page is not directly editable, reviews should be handled by our crawlers (if you see errors report them)!'
 		end
+		column :location do |review|
+			link_to "#{review.location.name}", admin_location_path(review.location)
+		end
 		column :id
 		column :author
 		column :author_url
