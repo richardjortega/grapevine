3e7292ddc08d1677970aad8c14d90d31d4b27386
Richard Ortega <richardjortega@gmail.com>
2013-01-11 23:03:22 -0600

    get source_location_uri for tripadvisor complete

--- lib/vineyard/tripadvisor.rb
+++ lib/vineyard/tripadvisor.rb
@@ -6,6 +6,16 @@ class TripAdvisor
 		@site = 'http://www.tripadvisor.com/'
 	end
 
+	def get_location_id(term, street_address, city, state, zip)
+		query = "#{term} #{street_address} #{city} #{state} #{zip}"
+		parsed_query = URI.parse(URI.encode(query.strip))
+		cx = "009410204525769731320:fiksaiphsou"
+		key = "AIzaSyAfzgIC3a-sxgoaFMZ7nZn9ioSZfwMenhM"
+		path = "https://www.googleapis.com/customsearch/v1?q=#{parsed_query}&cx=#{cx}&key=#{key}"
+		response = HTTParty.get(path)
+		location_id = response['items'][0]['link']
+	end
+
 	def get_new_reviews(latest_review, location_id)
 		begin
 		url = "#{@site}#{location_id}"
