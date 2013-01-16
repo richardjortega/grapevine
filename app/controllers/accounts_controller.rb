c896dcca1554e8cbfd69d6410531593126960227
Richard Ortega <richardjortega@gmail.com>
2013-01-16 14:45:36 -0600

    remove commented out code

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,10 +1,6 @@
 class AccountsController < ApplicationController
       force_ssl
 
-      # caches_action :index, :layout => false
-      # caches_action :show, :layout => false
-      # caches_action :billing, :layout => false
-
 	def index
             @plan = current_user.subscription.plan
             @location = current_user.locations[0]
