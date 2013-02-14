101c795d9ae717246ff9fa037812b5ac13a5abc5
Richard Ortega <richardjortega@gmail.com>
2013-02-14 06:37:03 -0600

    refactored track_api_call from opentable parser

--- lib/vineyard/opentable.rb
+++ lib/vineyard/opentable.rb
@@ -8,12 +8,8 @@ class Opentable
 		passkey = 'tjp43pshizud7jpex6rokvyop'
 		limit = 5
 		@url = "http://reviews.opentable.com/data/reviews.json?apiversion=#{apiversion}&passkey=#{passkey}&sort=submissiontime:desc&limit=#{limit}&filter=IsRatingsOnly:false&include=products&stats=reviews"
-		track_api_call('opentable')
-	end
-
-	def track_api_call(source_name)
-		source_id = Source.find_by_name("#{source_name}")
-		Source.update_counters(source_id, :api_count_daily => 1)
+		@source = Source.find_by_name('opentable')
+		track_api_call
 	end
 
 	def get_location_id(term, street_address, city, state, zip)
@@ -80,8 +76,14 @@ class Opentable
 		end
 	end
 
-	def get_new_reviews(latest_review, location_id)
+	def get_new_reviews(location, options = {})
 		begin
+		latest_review_date = options[:latest_review_date] || Date.today - 2
+		latest_comments = options[:latest_comments] || ''
+
+
+
+
 		@request = @url + URI.encode("&filter=ProductId:#{location_id}&RestaurantID=#{location_id}")
 		response = HTTParty.get(@request)
 
@@ -112,4 +114,44 @@ class Opentable
 
 		new_reviews
 	end
+
+	# def get_new_reviews(latest_review, location_id)
+	# 	begin
+	# 	@request = @url + URI.encode("&filter=ProductId:#{location_id}&RestaurantID=#{location_id}")
+	# 	response = HTTParty.get(@request)
+
+	# 	new_reviews = []
+	# 	response["Results"].each do |review|
+	# 		review_date = Date.strptime(review["AdditionalFields"][1]["Value"], "%m/%d/%Y")
+	# 		review_comment = review["ReviewText"].strip
+			
+	# 		# when review_date is taking date objects, change this to just 'if review_date >= latest_review[:post_date]'
+	# 		if review_date >= latest_review[:post_date]
+	# 			next if review_comment == latest_review[:comment].chomp
+	# 			new_review = {}
+	# 			new_review[:post_date] = review_date
+	# 			new_review[:comment] = review_comment
+	# 			new_review[:author] = 'OpenTable Diner'
+	# 			new_review[:rating] = review["Rating"].to_i
+	# 			new_review[:title] = review["Title"]
+	# 			new_review[:url] = "http://www.opentable.com/rest_profile.aspx?rid=#{location_id}&tab=2"
+	# 			new_reviews << new_review
+	# 		end
+	# 	end
+
+	# 	rescue => e
+	# 		pp e.message
+	# 		pp e.backtrace
+	# 		puts "Encountered error on #{@request} page, moving on..."
+	# 	end
+
+	# 	new_reviews
+	# end
+
+private
+	
+	def track_api_call
+		Source.update_counters(@source, :api_count_daily => 1)
+	end
+
 end
