44733969c29a90d59c154ece5a0ab4c67462a38c
Richard Ortega <richardjortega@gmail.com>
2013-02-14 04:08:16 -0600

    track_api_call for urbanspoon parser refactored

--- lib/vineyard/urbanspoon.rb
+++ lib/vineyard/urbanspoon.rb
@@ -6,12 +6,12 @@ require 'uri'
 class Urbanspoon
 	def initialize
 		@site = 'http://www.urbanspoon.com'
-		track_api_call('urbanspoon')
+		@source = Source.find_by_name('urbanspoon')
+		track_api_call
 	end
 
-	def track_api_call(source_name)
-		source_id = Source.find_by_name("#{source_name}")
-		Source.update_counters(source_id, :api_count_daily => 1)
+	def track_api_call
+		Source.update_counters(@source, :api_count_daily => 1)
 	end
 
 	def get_location_id(term, street_address, city, state, zip, lat, long)
