8f34eb4a396caa24707a1d3c77bdaf3e5d24444e
Josh Seltzer <joshseltzer@gmail.com>
2013-02-28 18:13:12 -0600

    sidebar now has all locations link pointing to the correct place and yielding correctly

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,12 @@ module ApplicationHelper
   def devise_mapping
     @devise_mapping ||= Devise.mappings[:user]
   end
+
+  def nav_link(link_text, link_path)
+  class_name = current_page?(link_path) ? 'active' : ''
+
+  content_tag(:li, :class => class_name) do
+    link_to link_text, link_path
+  end
+end
 end
