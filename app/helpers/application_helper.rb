1633a1f73bd4f1378d1de88d8a18c33ec06815be
Josh Seltzer <joshseltzer@gmail.com>
2013-03-09 17:23:22 -0600

    flash messages using bootstrap defaults, added to application help, created partial, put render in layouts/dashboard

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,10 +12,28 @@ module ApplicationHelper
   end
 
   def nav_link(link_text, link_path)
-  class_name = current_page?(link_path) ? 'active' : ''
+    class_name = current_page?(link_path) ? 'active' : ''
 
-  content_tag(:li, :class => class_name) do
-    link_to link_text, link_path
+    content_tag(:li, :class => class_name) do
+      link_to link_text, link_path
+    end
   end
+
+  def bootstrap_class_for flash_type
+    case flash_type
+      when :success
+        "alert-success"
+      when :error
+        "alert-error"
+      when :alert
+        "alert-block"
+      when :notice
+        "alert-info"
+      else
+        flash_type.to_s
+    end
+  end
+  
 end
-end
+
+
