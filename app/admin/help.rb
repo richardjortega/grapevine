0261f3b2573f485ddbd121eb05cf23d5051d3e70
Richard Ortega <richardjortega@gmail.com>
2013-03-09 22:50:56 -0600

    added ids for location and sources on admin pages

--- app/admin/help.rb
+++ app/admin/help.rb
@@ -47,7 +47,7 @@ ActiveAdmin.register_page "Help" do
                 'CqQBlQAAAIpI_j7Mt6PkTMnJ6SqsgoQ3wsVReoM8jSwJapt56TEfej1TG1sk0dSUxGsoljqHJB6NAmUOQ3GqLjZ53mwBFdvkYKMTkpbDaOz8SwOhFReia2r6hO_Rkvq02CCbinZM2COchOJI6OreV8bssKtrzsHAr4lMMHyiJLJvceG7R6OklLN4rWoyAMaazVnORTUQw10J_GprzyMLPoCAFCooXoUSEGVQge5CeRw1b53cP6M1vP8aFBam2IAUeVmC7nkV1LTT0CH5xCYQ'
             end
             td :style => 'border:1px solid black;' do
-                "Super simple now, one command with three required params [name with spaces/special characters/or whatever will be encoded, lat, long] : Example in terminal -- $ rake get_source_location_uri:google['paloma blanca',29.479975,-98.46389]"
+                "$ rake vineyard:get_source_location_uri:by_id[location_id,parser_id] -- where location_id is the Location's ID and parser_id is the Parser's ID (both as integers, no quotes)"
             end
         end
         tr do
@@ -61,7 +61,7 @@ ActiveAdmin.register_page "Help" do
                 'r/39/432569/restaurant/Riverwalk/Zinc-Bistro-Wine-Bar-San-Antonio'
             end
             td :style => 'border:1px solid black;' do
-                ''
+                "$ rake vineyard:get_source_location_uri:by_id[location_id,parser_id] -- where location_id is the Location's ID and parser_id is the Parser's ID (both as integers, no quotes)"
             end
         end
         tr do
@@ -75,7 +75,7 @@ ActiveAdmin.register_page "Help" do
                 'zinc-bistro-and-wine-bar-san-antonio'
             end
             td :style => 'border:1px solid black;' do
-                "Important: Make sure all the links you copy and paste are URI encoded. So special characters wont work, otherwise use $ rake get_source_location_uri:yelp['name whatever',lat,long]"
+                "$ rake vineyard:get_source_location_uri:by_id[location_id,parser_id] -- where location_id is the Location's ID and parser_id is the Parser's ID (both as integers, no quotes)"
             end
         end
         tr do
@@ -89,7 +89,7 @@ ActiveAdmin.register_page "Help" do
                 'Restaurant_Review-g60956-d437288-Reviews-Zinc_Champagne_Wine_Bar-San_Antonio_Texas.html'
             end
             td :style => 'border:1px solid black;' do
-                ''
+                "$ rake vineyard:get_source_location_uri:by_id[location_id,parser_id] -- where location_id is the Location's ID and parser_id is the Parser's ID (both as integers, no quotes)"
             end
         end
 
