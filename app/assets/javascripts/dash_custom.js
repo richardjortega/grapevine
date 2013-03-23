9e42a4dc3cce5358634cfb2ea1f73a9f11b4e65b
Richard Ortega <richardjortega@gmail.com>
2013-03-22 21:06:38 -0500

    dashboard now completely works cause of josh and we have line and pie chart

--- app/assets/javascripts/dash_custom.js
+++ app/assets/javascripts/dash_custom.js
@@ -1,5 +1,18 @@
 // dash custom js
 $(document).ready(function() {
 $('.show-popover').popover({ trigger: "hover" });
+
+
+var lineChartW = $('#last_two_weeks_reviews_line_chart').width();
+var lineCharts = $('[id*=_line_chart]');
+
+var pieChartW = $('#last_two_weeks_reviews_pie_chart').width();
+var pieCharts = $('[id*=_pie_chart]');
+
+$(lineCharts).width(lineChartW);
+$(pieCharts).width(pieChartW);
+
 });
+
+
 
