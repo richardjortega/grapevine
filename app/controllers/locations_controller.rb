c89fb10491190eea5375af25bc6a226da37a954b
Richard Ortega <richardjortega@gmail.com>
2013-03-22 19:46:56 -0500

    piechart added for two weeks reviews

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -17,44 +17,42 @@ class LocationsController < ApplicationController
   # GET /locations/1
   # GET /locations/1.json
   def show
-    #@location = Location.find(params[:id])
     @item = Location.find(params[:id])
-
-    # testing ton of locations
-    # user = User.find(106)
-    # @items = user.locations
-
+    @sources = Source.all
     @reviews = @item.reviews
 
-    @last_two_weeks_dates = (2.weeks.ago.to_date..Date.today).to_a #map(&:day) #.map(&:to_s)
-
     @last_two_weeks_reviews = @reviews.last_two_weeks_reviews
-
     @this_month_reviews = @reviews.this_month_reviews
     @last_month_reviews = @reviews.last_month_reviews
+
+    
 
     @line_chart = LazyHighCharts::HighChart.new('graph') do |f|
           f.options[:chart][:defaultSeriesType] = 'line'
           f.legend(:layout=> 'horizontal') 
           f.xAxis(:type => 'datetime')
-          Source.all.each do |source|
-            f.series(:pointInterval => 1.day, :pointStart => 2.weeks.ago.to_date, :name => source.name.capitalize, :data => locations_chart_data(2.weeks.ago.to_date..Date.today, @item, source).map(&:values).flatten.map {|value|value.to_i} )
+          @sources.each do |source|
+            f.series(:pointInterval => 1.day, :pointStart => 2.weeks.ago.to_date, :name => source.name.capitalize, :data => locations_chart_data(2.weeks.ago.to_date..Date.today, source).map(&:values).flatten.map {|value|value.to_i} )
           end
     end
 
-    @chart = LazyHighCharts::HighChart.new('pie') do |f|
+    @last_two_weeks_reviews_pie_chart_data = []
+    @sources.each do |source|
+      data_set = []
+      source_name = source.name.capitalize
+      total_reviews = @last_two_weeks_reviews.where("source_id = ?", source.id).count
+      data_set << source_name
+      data_set << total_reviews
+      @last_two_weeks_reviews_pie_chart_data << data_set
+    end
+
+    @pie_chart = LazyHighCharts::HighChart.new('pie') do |f|
       f.chart({:defaultSeriesType=>"pie" , :margin=> [0, 0, 0, 0]} )
-      f.colors(['#000099', '#cc0000', '#009900', '#0066ff', '#cccc99'])
+      # f.colors(['#000099', '#cc0000', '#009900', '#0066ff', '#cccc99'])
       series = {
                :type=> 'pie',
                :name=> 'Browser share',
-               :data=> [
-                  ['UrbanSpoon',   7],
-                  ['Yelp',       19],
-                  ['TripAdvisor',    5],
-                  ['Google+',     9],
-                  ['OpenTable',   0]
-               ]
+               :data=> @last_two_weeks_reviews_pie_chart_data
       }
       f.series(series)
       f.legend(:layout=> 'horizontal',:style=> {:left=> 'auto', :bottom=> 'auto',:right=> '50px',:top=> '100px'}) 
@@ -141,8 +139,9 @@ private
      @plan = current_user.subscription.plan
   end
 
-  def locations_chart_data(date_range, location, source)
-    reviews_by_day = location.reviews.where("source_id = ?", source.id).group("date(post_date)").select("post_date, count(id) as total_reviews").each_with_object({}) { |o,hsh| hsh[o.post_date.to_date] = o.total_reviews }
+  def locations_chart_data(date_range, source)
+    # @reviews instance variable set in the SHOW action
+    reviews_by_day = @reviews.where("source_id = ?", source.id).group("date(post_date)").select("post_date, count(id) as total_reviews").each_with_object({}) { |o,hsh| hsh[o.post_date.to_date] = o.total_reviews }
 
     #group_by { |l| l.post_date.to_date }
 
--- app/models/location.rb
+++ app/models/location.rb
@@ -35,12 +35,5 @@ class Location < ActiveRecord::Base
   def full_address
     [street_address, address_line_2, city, state, zip].compact.join(', ')
   end
-  
-  # def self.total_reviews_by_day(date_range, location, source)
-  #   orders = where(purchased_at: start.beginning_of_day..Time.zone.now)
-  #   orders = orders.group("date(purchased_at)")
-  #   orders = orders.select("purchased_at, sum(price) as total_price")
-  #   orders.group_by { |o| o.purchased_at.to_date }
-  # end
 
 end
